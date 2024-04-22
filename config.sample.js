394e7db4e2c75c3cc6ce885697e8b10c58e53090
diedrick <michael@bytestudios.com>
2024-04-22 16:02:28 -0500

    Creating real sample config js

--- config.sample.js
+++ config.sample.js
@@ -1,3 +1,8 @@
-// config.js
-export const apiRoot    = location.origin + '/wp-json/wp/v2/';
-export const acfApiRoot = location.origin + '/wp-json/acf/v3/';
+// global config variables for reporter and observer tools
+const CONFIG = {
+	directus_url: 'https://deserata.bytestudios.com',
+	shortcut_login_buttons: [
+		{name: 'Test Observer', email: 'testobserver@odeck.org', password: 'is_going_to_be_hard_to_guess'},
+	]
+};
+export { CONFIG };
