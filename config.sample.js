9273186e5ce56d4b80fce9b28171b6dd995a82b2
Michael Diedrick <michael@bytestudios.com>
2024-04-22 19:14:36 -0500

    More standard config

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,9 @@
 // global config variables for reporter and observer tools
 const CONFIG = {
-	directus_url: 'https://deserata.bytestudios.com',
+	directus_url: '', // usually empty, but could be offsite: 'https://deserata.bytestudios.com',
 	shortcut_login_buttons: [
-		{name: 'Test Observer', email: 'testobserver@odeck.org', password: 'is_going_to_be_hard_to_guess'},
+		// to make it easier to log in as a test observer, add shortcut buttons here
+		// {name: 'Test Observer', email: 'testobserver@odeck.org', password: 'is_going_to_be_hard_to_guess'},
 	]
 };
 export { CONFIG };
